73ad8a103690fe0557a747bbac06591832d0ff2a
Christian Egli <christian.egli@sbs.ch>
2013-11-17 21:05:19 +0100

    Initial import
    Create a small datomic application for students, their tests and scores.
    Meant as a learning and teaching project.

---
+++ README.md
@@ -0,0 +1,14 @@
+# students
+
+A small datomic database to model students, tests and scores.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2013 Christian Egli
+
+Distributed under the GNU General Public License either version 3 of
+the License, or (at your option) any later version.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject students "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "GNU General Public License"
+            :url "http://www.gnu.org/copyleft/gpl.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.datomic/datomic-free "0.8.4260"]])
